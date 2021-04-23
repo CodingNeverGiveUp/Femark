a2f048a209e613772e7b98836cc2dbbe7b63db9d
ojhdt <ojhdtmail@gmail.com>
2021-04-23 10:42:45 +0800

    add loginguide

---
+++ pages/guide/guide.js
@@ -0,0 +1,76 @@
+// pages/guide/guide.js
+const app = getApp()
+const database = require("../../utils/database.js")
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    theme: null,
+    primaryColor: app.globalData.primaryColor,
+    rgbaPrimaryColor: app.colorRgba(app.globalData.primaryColor, .2),
+    isPad: app.globalData.isPad,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    wx.onThemeChange((result) => {
+      // console.log(result)
+      this.setData({
+        theme: result.theme
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+})
