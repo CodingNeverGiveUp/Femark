c94454d83836eb95ca606f3a9c284b1794b2df50
ojhdt <ojhdtmail@gmail.com>
2021-05-09 20:32:57 +0800

    Update index.js

--- cloud/sendOne/index.js
+++ cloud/sendOne/index.js
@@ -10,7 +10,7 @@ exports.main = async (event, context) => {
     const hour = date.getHours()
     const minute = date.getMinutes()
     const second = date.getSeconds()
-  
+
     return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
   }
   var messages = []
@@ -20,58 +20,58 @@ exports.main = async (event, context) => {
   try {
     // 从云开发数据库中查询等待发送的消息列表
     await db
-    .collection('note').field({
-      task: true,
-      _openid: true,
-      _id: true,
-    }).get()
-    .then(res => {
-      console.log(res)
-      res.data.forEach((element, index) => {
-        element.task.forEach((innerElement,innerIndex) => {
-          if (innerElement.notification == true && innerElement.done == false && currenttime < innerElement.notificationTimestamp && currenttime + 300000 > innerElement.notificationTimestamp) {
-            let message = {
-              _openid: element._openid,
-              _id: element._id,
-              heading: innerElement.heading == null ? '' : innerElement.heading,
-              time: formatTime(new Date(innerElement.notificationTimestamp)),
-              urgency: "紧急且重要",
-              content: innerElement.content == null ? '' : innerElement.content,
-              reminderStatus: "待确认",
-              index: innerIndex,
+      .collection('note').field({
+        task: true,
+        _openid: true,
+        _id: true,
+      }).get()
+      .then(res => {
+        console.log(res)
+        res.data.forEach((element, index) => {
+          element.task.forEach((innerElement, innerIndex) => {
+            if (innerElement.notification == true && innerElement.done == false && currenttime < innerElement.notificationTimestamp && currenttime + 300000 > innerElement.notificationTimestamp) {
+              let message = {
+                _openid: element._openid,
+                _id: element._id,
+                heading: innerElement.heading == null ? '' : innerElement.heading,
+                time: formatTime(new Date(innerElement.notificationTimestamp)),
+                urgency: "紧急且重要",
+                content: innerElement.content == null ? '' : innerElement.content,
+                reminderStatus: "待确认",
+                index: innerIndex,
+              }
+              messages.push(message)
             }
-            messages.push(message)
-          }
+          })
         })
       })
-    })
 
     // 循环消息列表
     const sendPromises = messages.map(async message => {
       try {
         // 发送订阅消息
         await cloud.openapi.subscribeMessage.send({
-          touser:message._openid,//要推送用户的openid
+          touser: message._openid, //要推送用户的openid
           //page:'',
-          data:{//推送的内容
-            thing1:{
-              value: message.heading//'xx会议'
+          data: { //推送的内容
+            thing1: {
+              value: message.heading //'xx会议'
             },
-            time2:{
-              value:message.time//'2019年11月30日 21:00:00'
+            time2: {
+              value: message.time //'2019年11月30日 21:00:00'
             },
-            phrase9:{
-              value:message.urgency//'紧急且重要'
+            phrase9: {
+              value: message.urgency //'紧急且重要'
             },
-            thing4:{
-              value:message.content//'会议内容为.....'
+            thing4: {
+              value: message.content //'会议内容为.....'
             },
-            phrase8:{
-              value:message.reminderStatus//'待确认'
+            phrase8: {
+              value: message.reminderStatus //'待确认'
             }
           },
-            templateId:'n5ZgQ_uHeZFwKecg8S_WjDb3Gfx7a9BUTZbkLPnWTXI'//模板id
-          })
+          templateId: 'n5ZgQ_uHeZFwKecg8S_WjDb3Gfx7a9BUTZbkLPnWTXI' //模板id
+        })
         // 发送成功后将消息的状态改为已发送
         return db
           .collection('note')
@@ -83,7 +83,7 @@ exports.main = async (event, context) => {
               }
             },
           });
-        
+
       } catch (e) {
         return e;
       }
